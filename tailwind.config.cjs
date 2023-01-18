fe5fe4c8a7f023eabc80ebaf1563641dc1db9418
Gabriel Gomes <gabrielgomes98@gmail.com>
2023-01-18 18:11:24 -0300

    feat: new prop to grid on tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,3 +1,5 @@
+
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -8,6 +10,9 @@ module.exports = {
     extend: {
       colors:{
         background: '#09090A'  
+      },
+      gridTemplateRows:{
+        7: 'repeat(7,minmax(0, 1fr))'
       }
     },
   },
