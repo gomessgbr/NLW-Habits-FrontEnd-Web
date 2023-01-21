573de25bb4a0f802852b6d4f137bdc0d9e15b32b
Gabriel Gomes <gabrielgomes98@gmail.com>
2023-01-21 13:14:12 -0300

    feat: animation on buttons and checkbox

--- src/components/NewHabitForm.tsx
+++ src/components/NewHabitForm.tsx
@@ -17,18 +17,18 @@ export function NewHabitForm() {
   const [title, setTitle] = useState("");
   const [weekDays, setWeekDays] = useState<number[]>([]);
 
- async  function createNewHabit(event: FormEvent) {
+  async function createNewHabit(event: FormEvent) {
     event.preventDefault();
     if (!title || weekDays.length === 0) {
-      return
+      return;
     }
-    await api.post('habits', {
+    await api.post("habits", {
       title,
-      weekDays
-    })
-    setTitle('')
-    setWeekDays([])
-    alert('Hábito criado com sucesso')
+      weekDays,
+    });
+    setTitle("");
+    setWeekDays([]);
+    alert("Hábito criado com sucesso");
   }
   function handleToggleWeekDay(weekDay: number) {
     if (weekDays.includes(weekDay)) {
@@ -49,7 +49,7 @@ export function NewHabitForm() {
         type="text"
         id="title"
         placeholder="ex.: Exercícios, dormir bem, etc..."
-        className="p-4 rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-zinc-400 "
+        className="p-4 rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-zinc-400  group-focus:ring-2  group-focus:ring-violet-700 group-focus:ring-offset-2 group-focus:ring-offset-zinc-900"
         autoFocus
         onChange={(event) => setTitle(event.target.value)}
         value={title}
@@ -67,7 +67,7 @@ export function NewHabitForm() {
               checked={weekDays.includes(index)}
               onCheckedChange={() => handleToggleWeekDay(index)}
             >
-              <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2  border-zinc-800 group-data-[state=checked]:bg-green-500  group-data-[state=checked]:border-green-500">
+              <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2  border-zinc-800 group-data-[state=checked]:bg-green-500  group-data-[state=checked]:border-green-500 transition-colors  ">
                 <Checkbox.Indicator>
                   <Check size={20} className="text-white" />
                 </Checkbox.Indicator>
@@ -82,7 +82,7 @@ export function NewHabitForm() {
 
       <button
         type="submit"
-        className="mt-6 rounded-lg p-4 gap-3 flex justify-center  items-center font-semibold bg-green-600 hover:bg-green-500 "
+        className="mt-6 rounded-lg p-4 gap-3 flex justify-center  items-center font-semibold bg-green-600 hover:bg-green-500 transition-colors  focus:outline-none focus:ring-2  focus:ring-green-600 focus:ring-offset-2 focus:ring-offset-zinc-900 "
       >
         <Check size={20} weight="bold" />
         Confirmar
